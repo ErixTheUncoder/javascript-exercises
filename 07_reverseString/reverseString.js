d5f5ab7a1428b42760e3f1fb91953567590f221f
ErixTheUncoder <erichong.0969@gmail.com>
2025-08-21 11:39:33 +0800

    test.html changes

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  const reversedText = text.split("").reverse().join("");
+  return reversedText;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+  
